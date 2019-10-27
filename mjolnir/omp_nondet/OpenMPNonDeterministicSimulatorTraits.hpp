8947557e27aef44e10eec6bb28e79da81655e90f
ToruNiina <niina.toru.68u@gmail.com>
2019-10-27 19:39:59 +0900

    feat: add a tag for Nondeterministic OpenMP

---
+++ mjolnir/omp_nondet/OpenMPNonDeterministicSimulatorTraits.hpp
@@ -0,0 +1,28 @@
+#ifndef MJOLNIR_OMP_OPENMP_NONDETERMINISTIC_SIMULATOR_TRAITS
+#define MJOLNIR_OMP_OPENMP_NONDETERMINISTIC_SIMULATOR_TRAITS
+#include <mjolnir/math/Vector.hpp>
+
+namespace mjolnir
+{
+
+template<typename realT, template<typename, typename> class boundaryT>
+struct OpenMPNonDeterministicSimulatorTraits
+{
+    using real_type       = realT;
+    using coordinate_type = math::Vector<real_type, 3>;
+
+    using matrix33_type = math::Matrix<real_type, 3, 3>;
+    using matrix44_type = math::Matrix<real_type, 4, 4>;
+
+    using boundary_type = boundaryT<real_type, coordinate_type>;
+};
+
+template<typename T>
+struct is_openmp_nondeterministic_simulator_traits : std::false_type{};
+
+template<typename realT, template<typename, typename> class boundaryT>
+struct is_openmp_nondeterministic_simulator_traits<OpenMPNonDeterministicSimulatorTraits<realT, boundaryT>>: std::true_type{};
+
+
+} // mjolnir
+#endif /* MJOLNIR_DEFAULT_TRAITS */
