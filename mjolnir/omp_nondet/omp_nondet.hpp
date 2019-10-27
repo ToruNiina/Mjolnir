77dd708c3661ce314e50b9a364fa1f217ae9dd51
ToruNiina <niina.toru.68u@gmail.com>
2019-10-27 20:11:44 +0900

    feat: add all-in-one include file to omp_nondet

---
+++ mjolnir/omp_nondet/omp_nondet.hpp
@@ -0,0 +1,11 @@
+#ifndef MJOLNIR_OMP_NONDET_OMP_NONDET_HPP
+#define MJOLNIR_OMP_NONDET_OMP_NONDET_HPP
+
+#include <mjolnir/omp_nondet/GlobalPairInteraction.hpp>
+#include <mjolnir/omp_nondet/GlobalPairLennardJonesInteraction.hpp>
+#include <mjolnir/omp_nondet/OpenMPNonDeterministicSimulatorTraits.hpp>
+#include <mjolnir/omp_nondet/PeriodicGridCellList.hpp>
+#include <mjolnir/omp_nondet/RandomNumberGenerator.hpp>
+#include <mjolnir/omp_nondet/UnlimitedGridCellList.hpp>
+
+#endif// MJOLNIR_OMP_NONDET_OMP_NONDET_HPP
