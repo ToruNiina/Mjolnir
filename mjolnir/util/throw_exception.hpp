da7237d9dc479f6c14ae1f132523b79af5b2fd07
ToruNiina <niina.toru.68u@gmail.com>
2021-04-30 14:08:58 +0900

    refactor: use fold expression

--- mjolnir/util/throw_exception.hpp
+++ mjolnir/util/throw_exception.hpp
@@ -1,42 +1,17 @@
 #ifndef MJOLNIR_UTIL_THROW_EXCEPTION_HPP
 #define MJOLNIR_UTIL_THROW_EXCEPTION_HPP
-#include <utility>
 #include <sstream>
 #include <string>
 
 namespace mjolnir
 {
-namespace detail
-{
-
-inline void concat_args_to_string_impl(std::ostringstream&)
-{
-    return;
-}
-
-template<typename Arg1, typename ...Args>
-void concat_args_to_string_impl(std::ostringstream& oss,
-        Arg1&& arg1, Args&& ... args)
-{
-    oss << std::forward<Arg1>(arg1);
-    concat_args_to_string_impl(oss, std::forward<Args>(args)...);
-    return;
-}
-
-template<typename ...Args>
-std::string concat_args_to_string(Args&& ... args)
-{
-    std::ostringstream oss;
-    concat_args_to_string_impl(oss, std::forward<Args>(args)...);
-    return oss.str();
-}
-
-} // detail
 
 template<typename Exception, typename ...Args>
 [[noreturn]] void throw_exception(Args&& ... args)
 {
-    throw Exception(detail::concat_args_to_string(std::forward<Args>(args)...));
+    std::ostringstream oss;
+    (oss << ... << args);
+    throw Exception(oss.str());
 }
 
 } // mjolnir
